87d87d97d8360c8a24410e10ed04116bc9abf0e7
ArconRM <85395885+ArconRM@users.noreply.github.com>
2021-10-02 18:56:42 +0400

    Добавьте файлы проекта.

---
+++ ProGame.Hospital.Common.Entities/Doctor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProGame.Hospital.Common.Entities
+{
+    public enum Specialities
+    {
+        Dentist,
+        Surgeon,
+        Psychiatrist,
+        Oncologist
+    }
+    public class Doctor
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public IEnumerable<Specialities> speciality;
+        public Timetable timetable { get; set; }
+    }
+}
