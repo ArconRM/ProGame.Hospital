7f0c1262f4634bc242036cdc645aeeb322ff3bec
ArconRM <85395885+ArconRM@users.noreply.github.com>
2021-10-16 22:59:30 +0400

    Конструкторы

--- ProGame.HospitalAPI.Common.Entities/Appointment.cs
+++ ProGame.HospitalAPI.Common.Entities/Appointment.cs
@@ -8,6 +8,12 @@ namespace ProGame.HospitalAPI.Common.Entities
 {
     public class Appointment: BaseEntity
     {
+        public Appointment(Record record, string description, bool isCancelled)
+        {
+            Record = record;
+            Description = description;
+            IsCancelled = isCancelled;
+        }
         public Record Record { get; set; }
         public string Description { get; set; }
         public bool IsCancelled { get; set; }
--- ProGame.HospitalAPI.Common.Entities/Doctor.cs
+++ ProGame.HospitalAPI.Common.Entities/Doctor.cs
@@ -8,6 +8,14 @@ namespace ProGame.HospitalAPI.Common.Entities
 {
     public class Doctor: BaseEntity
     {
+        public Doctor(string fullName, string phoneNumber, string email, Specialities speciality, IEnumerable<Record> records)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Speciality = speciality;
+            Records = records;
+        }
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
