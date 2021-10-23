9cf7cc6bb994d8c49550bb9c1ae0f8a05078ac54
ArconRM <85395885+ArconRM@users.noreply.github.com>
2021-10-23 19:08:02 +0400

    ugysdfkjhds

--- ProGame.HospitalAPI.Common.Entities/Patient.cs
+++ ProGame.HospitalAPI.Common.Entities/Patient.cs
@@ -8,19 +8,17 @@ namespace ProGame.HospitalAPI.Common.Entities
 {
     public class Patient: BaseEntity
     {
-        public Patient(string fullName, string phoneNumber, string email, IEnumerable<Appointment> appointments, IEnumerable<Record> records)
+        public Patient(string fullName, string phoneNumber, string email, IEnumerable<Appointment> appointments)
         {
             FullName = fullName;
             PhoneNumber = phoneNumber;
             Email = email;
             Appointments = appointments;
-            Records = records;
         }
 
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public IEnumerable<Appointment> Appointments { get; set; }
-        public IEnumerable<Record> Records { get; set; }
     }
 }
